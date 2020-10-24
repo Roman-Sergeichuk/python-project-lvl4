c7e4bf8119215fd0e5b8759d257b5f012a3ba681
Roman-Sergeichuk <sergeichuk.roman@mail.ru>
2020-10-25 00:38:02 +0300

    Registration and authorization added

---
+++ task_manager/views.py
@@ -0,0 +1,13 @@
+from django.contrib.auth.forms import UserCreationForm
+from django.urls import reverse_lazy
+from django.views.generic.edit import FormView
+
+
+class RegisterFormView(FormView):
+    form_class = UserCreationForm
+    success_url = reverse_lazy('index')
+    template_name = "registration/register.html"
+
+    def form_valid(self, form):
+        form.save()
+        return super(RegisterFormView, self).form_valid(form)
