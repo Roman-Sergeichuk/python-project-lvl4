e84af70e5700b94f9508f12a71a3807696a2dae1
Roman-Sergeichuk <sergeichuk.roman@mail.ru>
2020-10-30 21:42:23 +0300

    Create tag test added

--- tasks/tests.py
+++ tasks/tests.py
@@ -3,8 +3,6 @@ from django.urls import reverse
 from tasks.models import Tag, Task, TaskStatus
 from django.contrib.auth.models import User
 from django.test.client import Client
-from django.utils import timezone
-from django.contrib.auth import SESSION_KEY
 from django.urls import reverse, reverse_lazy
 from django.shortcuts import get_object_or_404
 
@@ -16,12 +14,19 @@ STATUS_UPDATE_URL_NAME = 'update_status'
 STATUS_DELETE_URL_NAME = 'delete_status'
 STATUS_DATA = {'name': 'new'}
 STATUS_NAME = STATUS_DATA['name']
+TAG_CREATE_URL = reverse_lazy('create_tag')
+TAG_DATA = {'name': 'Новая'}
+TAG_NAME = TAG_DATA['name']
+TASK_CREATE_URL = reverse_lazy('create_task')
+TASK_DATA = {'name': 'task_1', 'description': 'description for task 1'}
+TASK_UPDATE_URL_NAME = 'update_task'
+
 USERNAME = REGISTRATION_DATA['username']
 PASSWORD = REGISTRATION_DATA['password1']
 LOGIN_DATA = {'username': USERNAME, 'password': PASSWORD}
 
 
-class TaskManagerTestCase(TestCase):
+class TestRegisterAndLogin(TestCase):
 
     def test_register(self):
         client = Client()
@@ -37,12 +42,15 @@ class TaskManagerTestCase(TestCase):
         self.assertEqual(response.status_code, 200)
         self.assertTrue(response.context['user'].is_authenticated)
 
+
+class TestStatusCRUD(TestCase):
+
     def test_create_status(self):
         client = Client()
         User.objects.create_user(username=USERNAME, password=PASSWORD)
         client.post(LOGIN_URL, LOGIN_DATA, follow=True)
         response = client.post(STATUS_CREATE_URL, STATUS_DATA)
-        status_in_db = TaskStatus.objects.get(pk=1)
+        status_in_db = TaskStatus.objects.get(name=STATUS_NAME)
         self.assertEqual(response.status_code, 302)
         self.assertEqual(STATUS_NAME, status_in_db.name)
 
@@ -66,13 +74,65 @@ class TaskManagerTestCase(TestCase):
         old_status = TaskStatus.objects.get(name='old_name')
         status_url = reverse(STATUS_DELETE_URL_NAME, args=[str(old_status.pk)])
         response = client.post(status_url)
-        statuses = TaskStatus.objects.all()
-        self.assertEqual(statuses.count, 0)
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(len(TaskStatus.objects.all()), 0)
 
 
+class TestTagCreate(TestCase):
+
+    def test_create_tag(self):
+        client = Client()
+        User.objects.create_user(username=USERNAME, password=PASSWORD)
+        client.post(LOGIN_URL, LOGIN_DATA, follow=True)
+        response = client.post(TAG_CREATE_URL, TAG_DATA)
+        tag_in_db = Tag.objects.get(name=TAG_NAME)
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(TAG_NAME, tag_in_db.name)
 
 
+class TestTaskCRUD(TestCase):
 
+    def test_create_task(self):
+        client = Client()
+        User.objects.create_user(username=USERNAME, password=PASSWORD)
+        client.post(LOGIN_URL, LOGIN_DATA, follow=True)
+        TaskStatus.objects.create(name=STATUS_NAME)
+        Tag.objects.create(name=TAG_NAME)
+        status = TaskStatus.objects.get(name=STATUS_NAME)
+        tag = Tag.objects.get(name=TAG_NAME)
+        user = User.objects.get(username=USERNAME)
+        response = client.post(TASK_CREATE_URL, {'name': 'task_1', 'description': 'description for task 1', 'status': status.pk, 'tags': tag.pk, 'Assigned_to': user.pk})
+        print(response)
+        # task_in_db = Task.objects.get(name='task_1')
+        # self.assertEqual(response.status_code, 302)
+        # self.assertEqual('task_1', task_in_db.name)
+
+    def test_update_task(self):
+        client = Client()
+        User.objects.create_user(username=USERNAME, password=PASSWORD)
+        client.post(LOGIN_URL, LOGIN_DATA, follow=True)
+        TaskStatus.objects.create(name=STATUS_NAME)
+        Tag.objects.create(name=TAG_NAME)
+        Task.objects.create(name='task1',
+                            description='description',
+                            status=TaskStatus.objects.get(name=STATUS_NAME),
+                            creator=User.objects.get(username=USERNAME),
+                            assigned_to=User.objects.get(username=USERNAME)
+                            )
+        old_task = Task.objects.get(name='task1')
+        task_url = reverse(TASK_UPDATE_URL_NAME, args=[str(old_task.pk)])
+        task_update_data = {
+            'name': 'task2',
+            'description': 'description',
+            'status': str(TaskStatus.objects.get(name=STATUS_NAME)),
+            'assigned_to': str(User.objects.get(username=USERNAME))
+        }
+        response = client.post(task_url, task_update_data)
+        task_in_db = TaskStatus.objects.get(pk=old_task.pk)
+        print(task_in_db)
+
+        # self.assertEqual(response.status_code, 302)
+        # self.assertEqual(STATUS_NAME, status_in_db.name)
 
 
 
